71b68a560a875ac0bd044cbd0e8b7436e0090fca
GabrielGustafsonUIC <60358481+GabeGustafson@users.noreply.github.com>
2022-05-03 18:47:02 -0500

    unicafe 1

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,12 +1,11 @@
 import { useState } from 'react'
 
-const FeedbackButton = ({}) => {
-  // TODO
-}
+const FeedbackButton = ({text, count, setCount}) => 
+  <button onClick = {() => setCount(count+1)}>{text}</button>
 
-const FeedbackDisplay = ({}) => {
-  // TODO
-}
+
+const FeedbackDisplay = ({text, count}) => 
+  <p>{text} {count}</p>
 
 const App = () => {
   // save clicks of each button to its own state
@@ -14,9 +13,26 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const goodText = "good"
+  const neutralText = "neutral"
+  const badText = "bad"
+
   return (
     <div>
-      code here
+
+      <div>
+        <h1>give feedback</h1>
+        <FeedbackButton text={goodText} count={good} setCount={setGood}/>
+        <FeedbackButton text={neutralText} count={neutral} setCount={setNeutral}/>
+        <FeedbackButton text={badText} count={bad} setCount={setBad}/>
+      </div>
+
+      <div>
+        <h1>statistics</h1>
+        <FeedbackDisplay text={goodText} count={good}/>
+        <FeedbackDisplay text={neutralText} count={neutral}/>
+        <FeedbackDisplay text={badText} count={bad}/>
+      </div>
     </div>
   )
 }
