40723c55db837f8b679ca1c41f35d763c7eca940
GabrielGustafsonUIC <60358481+GabeGustafson@users.noreply.github.com>
2022-05-10 14:06:17 -0500

    anecdotes

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,11 +1,36 @@
 import { useState } from 'react'
 
-const Button = ({setter, max}) => {
-  const rand_ind = Math.floor(Math.random() * max);
-  const handler = () => setter(rand_ind)
+const RandomButton = ({setter, max}) => {
+  const handler = () => {
+    const rand_ind = Math.floor(Math.random() * max);
+    setter(rand_ind);
+  }
 
-  return <button onClick={handler}>Next Anecdote</button>
-}
+  return <button onClick={handler}>Next Anecdote</button>;
+};
+
+const VoteButton = ({setter, selected, votes}) => 
+{
+  const handler = () => {
+    const newVotesData = [...(votes.data)];
+    newVotesData[selected] += 1;
+
+    const newHighest = (newVotesData[selected] > (votes.data)[votes.highest]) ? 
+                        selected : votes.highest;
+
+    setter({data: newVotesData, highest: newHighest});
+  };
+
+  return <button onClick={handler}>Vote</button>;
+};
+
+
+const AnecdoteDisplay = ({title, anecdotes, votes, index}) =>
+  <div>
+    <h1>{title}</h1>
+    {anecdotes[index]}
+    <p>has {votes.data[index]} votes</p>
+  </div>;
 
 const App = () => {
   const anecdotes = [
@@ -16,20 +41,25 @@ const App = () => {
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
-  ]
-   
-  const [selected, setSelected] = useState(0)
+  ];
+  const num_anecdotes = anecdotes.length;
 
-  const rand_ind = Math.floor(Math.random() * anecdotes.length);
+  const [votes, setVotes] = useState({
+    data: Array(num_anecdotes).fill(0),
+    highest: 0
+  });
+
+  const [selected, setSelected] = useState(0);
 
   return (
     <>
+    <AnecdoteDisplay title="Anecdote of the day" anecdotes={anecdotes} votes={votes} index={selected}/>
     <div>
-      {anecdotes[selected]}
+      <VoteButton setter={setVotes} selected={selected} votes={votes}/>
+      <RandomButton setter={setSelected} max={num_anecdotes}/>
     </div>
-    <div>
-      <Button setter={setSelected} max={anecdotes.length}/>
-    </div>
+
+    <AnecdoteDisplay title="Anecdote with most votes" anecdotes={anecdotes} votes={votes} index={votes.highest}/>
     </>
   )
 }
